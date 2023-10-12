f825bd8b9b78fc7563adcc82e7c23d708959e3b6
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-10-12 19:10:01 +0100

    First failing test

---
+++ test_integer_colour.py
@@ -0,0 +1,24 @@
+import unittest
+
+"""
+Task
+Given an integer x perform the following conditional actions:
+
+If x is odd, return 'Red'
+If x is even and in the inclusive range of 2 to 5, return 'Blue'
+If x is even and in the inclusive range of 6 to 20, return 'Red'
+If x is even and greater than 20, return 'Blue'
+"""
+
+
+class TestIntegerColour(unittest.TestCase):
+    def test_works_for_odd(self):
+        expected = 'Red'
+
+        actual = integer_colour(5)
+
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
