6f9d1187e3c7c789969fb71b1a3ae74ca6dc24c5
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-10-12 20:17:25 +0100

    Make reading a file test work

--- increment_line_number.py
+++ increment_line_number.py
@@ -1,5 +1,6 @@
 def read_file(file_name):
-    pass
+    with open(file_name, 'r') as file:
+        return [line.rstrip() for line in file.readlines()]
 
 def increment_line_number(file_name):
     pass
