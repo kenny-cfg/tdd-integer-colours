699ee934c837fb759e5d74868b143e7d25e42401
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-10-12 20:36:47 +0100

    Mocking example

--- increment_line_number.py
+++ increment_line_number.py
@@ -2,5 +2,8 @@ def read_file(file_name):
     with open(file_name, 'r') as file:
         return [line.rstrip() for line in file.readlines()]
 
+
 def increment_line_number(file_name):
-    pass
+    file_contents = read_file(file_name)
+    number = int(file_contents.pop().split('.')[0])
+    return number + 1
--- test_increment_line_number.py
+++ test_increment_line_number.py
@@ -1,6 +1,7 @@
 import unittest
+from unittest import mock
 
-from increment_line_number import read_file
+from increment_line_number import read_file, increment_line_number
 
 
 class TestIncrementLineNumber(unittest.TestCase):
@@ -11,10 +12,32 @@ class TestIncrementLineNumber(unittest.TestCase):
 
         self.assertEqual(expected, actual)
 
-    def it_works(self):
-        pass
-        # Mock to return some predefined content
-        # Make assertion
+    @mock.patch('increment_line_number.read_file')
+    def test_increment_line_number_works(self, read_file_mock):
+        content = [
+            '1. Hello',
+            '2. Hi',
+            '3. Good morning'
+        ]
+        read_file_mock.return_value = content
+        expected = 4
+
+        actual = increment_line_number('some_file')
+
+        self.assertEqual(expected, actual)
+
+    @mock.patch('increment_line_number.read_file')
+    def test_increment_line_number_works_with_two_items(self, read_file_mock):
+        content = [
+            '1. Hello',
+            '2. Hi',
+        ]
+        read_file_mock.return_value = content
+        expected = 3
+
+        actual = increment_line_number('some_file')
+
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
